23fcad77c83c302fbdf95e2b1378ca51d872b8c9
Bartłomiej Sęk <bartlomiej.sek@bitbag.pl>
2024-12-16 13:41:15 +0100

    Lint project files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,20 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-  js.configs.recommended,
-  {
-    ignores: ['vendor/', 'node_modules/', 'public/'],
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      }
+    js.configs.recommended,
+    {
+        ignores: ['vendor/', 'node_modules/', 'public/'],
     },
-    rules: {
-      indent: ['error', 4],
-      semi: ['error', 'always'],
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            }
+        },
+        rules: {
+            indent: ['error', 4],
+            semi: ['error', 'always'],
+        }
     }
-  }
 ];
