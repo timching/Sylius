d1e031ed4e836eca8bf697d827940a3e6074b9d5
Bartłomiej Sęk <bartlomiej.sek@bitbag.pl>
2024-12-17 09:55:10 +0100

    Add eslint rules, add eslint script to package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,18 +4,20 @@ import globals from 'globals';
 export default [
     js.configs.recommended,
     {
-        ignores: ['vendor/', 'node_modules/', 'public/'],
+        ignores: ['vendor/', 'node_modules/', 'public/']
     },
     {
         languageOptions: {
             globals: {
                 ...globals.browser,
-                ...globals.node,
+                ...globals.node
             }
         },
         rules: {
-            indent: ['error', 4],
-            semi: ['error', 'always'],
+            'indent': ['error', 4],
+            'semi': ['error', 'always'],
+            'comma-dangle': ['error', 'never'],
+            'quotes': ['error', 'single']
         }
     }
 ];
